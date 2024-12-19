5fec3975da67b2691e287cc3778190dbc0623b3f
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-12-19 15:39:09 +0200

    Add clean_data.py for preprocessing terrorism data
    - Introduced a new script, clean_data.py, to clean and preprocess terrorism data.
    - Implemented a function to select relevant columns, rename them for clarity, and handle negative values in numeric fields by replacing them with NaN.
    - Added a main block to read raw data from a CSV file and save the cleaned data to a new CSV file.

---
+++ src/data_etl/transform/clean_data.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+def clean_terrorism_data(df):
+    cleaned_df = df[[
+        'eventid', 
+        'iyear', 'imonth', 'iday',
+        'country_txt', 'region_txt', 'provstate', 'city',
+        'latitude', 'longitude',
+        'attacktype1_txt',
+        'nkill', 'nwound',
+        'target1', 'summary', 'gname',
+        'targtype1_txt', 'weaptype1_txt',
+        'motive',
+        'success', 'nperps'
+    ]].copy()
+    
+    column_mapping = {
+        'iyear': 'Year',
+        'imonth': 'Month',
+        'iday': 'Day',
+        'country_txt': 'Country',
+        'region_txt': 'Region',
+        'provstate': 'State',
+        'attacktype1_txt': 'AttackType',
+        'nkill': 'Killed',
+        'nwound': 'Wounded',
+        'target1': 'Target',
+        'latitude': 'Latitude',
+        'city': 'City',
+        'longitude': 'Longitude',
+        'gname': 'Group',
+        'summary': 'Summary',
+        'motive': 'Motive',
+        'success': 'Success',
+        'nperps': 'Nperps',
+        'targtype1_txt': 'Target_type',
+        'weaptype1_txt': 'Weapon_type'
+    }
+    cleaned_df = cleaned_df.rename(columns=column_mapping)
+    numeric_columns = ['Killed', 'Wounded', 'Nperps']
+    for col in numeric_columns:
+        cleaned_df.loc[cleaned_df[col] < 0, col] = np.nan
+    
+    return cleaned_df
+
+if __name__ == "__main__":
+    df = pd.read_csv('csv_data/globalterrorismdb_raw.csv', encoding='latin1', low_memory=False)
+    clean_df = clean_terrorism_data(df)
+    clean_df.to_csv('csv_data/globalterrorismdb_clean.csv', index=False)
