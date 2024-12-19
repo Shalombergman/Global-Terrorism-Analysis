e299a00a967f31eba2e04b778fb4f2defa42000a
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-12-19 21:13:58 +0200

    Refactor settings management in settings.py
    - Updated the Settings class to load PostgreSQL configuration from environment variables using dotenv.
    - Removed hardcoded database connection parameters and replaced them with environment variable assignments for better security and flexibility.
    - Cleaned up the code by removing unused Neo4j properties and configuration.
    
    This change enhances the application's configuration management, allowing for easier deployment and environment-specific settings.

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,30 +1,20 @@
-from pydantic_settings import BaseSettings
 from functools import lru_cache
+from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 class Settings(BaseSettings):
-    # PostgreSQL
-    POSTGRES_HOST: str
-    POSTGRES_PORT: int
-    POSTGRES_DB: str
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    
-    # Neo4j
-    NEO4J_HOST: str
-    NEO4J_PORT: int
-    NEO4J_USER: str
-    NEO4J_PASSWORD: str
-    
+    POSTGRES_HOST: str = os.getenv('POSTGRES_HOST')
+    POSTGRES_PORT: str = os.getenv('POSTGRES_PORT')
+    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
+    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
+
     @property
     def POSTGRES_URL(self) -> str:
         return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-    
-    @property
-    def NEO4J_URL(self) -> str:
-        return f"neo4j://{self.NEO4J_HOST}:{self.NEO4J_PORT}"
-    
-    class Config:
-        env_file = ".env"
 
 @lru_cache
 def get_settings() -> Settings:
