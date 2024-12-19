acacec2fce5953b2180c4676d3d0e1cefd819b94
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-12-19 21:14:51 +0200

    Add init_db.py for database initialization
    - Introduced a new script, init_db.py, to initialize the PostgreSQL database.
    - The script creates a database engine and sets up the schema by creating all tables defined in the models.
    - This addition supports the setup of the database schema for the application, enhancing the ETL process.

---
+++ src/data_etl/load/init_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from src.db.models.base import Base
+from src.db.models.attack_types import AttackType
+from src.db.models.groups import Group
+from src.db.models.regions import Region
+from src.db.models.targets import Target
+from src.db.models.terror_events import TerrorEvent
+from src.db.models.weapon_types import WeaponType
+
+def init_db(engine):
+    # יצירת כל הטבלאות
+    Base.metadata.create_all(bind=engine)
